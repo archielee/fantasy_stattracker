367f6e3c3aaab99e946c06a791ba173c5a47fd35
Archie Lee <archie_lee7@hotmail.com>
2021-03-19 17:44:51 -0400

    Add option to pass in key file path to setup script and add instruction to README

--- scripts/initial_setup.py
+++ scripts/initial_setup.py
@@ -1,8 +1,9 @@
+import argparse
 import pprint
 from fantasy_stattracker.web.api_access import YahooAPI
 
-def main():
-    api = YahooAPI()
+def main(args):
+    api = YahooAPI(auth_key_file=args.key_file)
     api.login()
     test_url = "https://fantasysports.yahooapis.com/fantasy/v2/"
     r = api.get(test_url)
@@ -10,4 +11,9 @@ def main():
     pprint.pprint(r)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("Yahoo API Key Set-up")
+    parser.add_argument("--key_file", "-f", type=str,
+                        default="auth_key.json",
+                        help="Full path to API key file")
+    args = parser.parse_args()
+    main(args)
